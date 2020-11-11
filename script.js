2e1ee11ecd63e29d757f571c3ceab962efb12e82
Ryan-Henson <72321799+Ryan-Henson@users.noreply.github.com>
2020-11-10 18:26:40 -0600

    able to set up consolelog for simble api

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-var rainyBoi = 
-"https://api.openweathermap.org/data/2.5/weather?q="
-+ {city name} + "&appid=" + {API key}
+let appID = "a11a01cd130954eda60a77225c0c9093";
+let city = "Dallas";
 
+var rainyBoi =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  appID;
+
+var req = new Request(rainyBoi);
+fetch(req)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+  });
