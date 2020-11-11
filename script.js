1289780df55d7f1c6aab4448739a574bdf01331e
Ryan-Henson <72321799+Ryan-Henson@users.noreply.github.com>
2020-11-10 18:59:11 -0600

    changed links to bootstrap to work. added fetch request and function to pull info from api, only working for set city need to update to responed with inputed city, also set up temp conversion on request per activity.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let appID = "a11a01cd130954eda60a77225c0c9093";
 let city = "Dallas";
+let current_date = moment().format("L");
 
 var rainyBoi =
   "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -12,4 +13,22 @@ fetch(req)
   .then((response) => response.json())
   .then((response) => {
     console.log(response);
+
+    function lookOutSide() {
+      let temp = (response.main.temp - 273.15) * (9 / 5) + 32;
+      let windspeed = response.wind.speed * 2.237;
+
+      console.log(temp);
+
+      $("#current-city").text(response.name + " " + current_date);
+      $("#weather-img").attr(
+        "src",
+        "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+      );
+      $("#temperature").text(temp.toFixed(2) + "°F");
+      $("#humidity").text(response.main.humidity + "%");
+      $("#windspeed").text(windspeed.toFixed(2) + " " + "mph");
+    }
+
+    $(".btn").click(lookOutSide);
   });
